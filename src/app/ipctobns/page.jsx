e8ec1d5c9e15d54175d8c876be7b51d8c97147be
anmoljain16 <ajanmoldosi@gmail.com>
2024-06-30 04:50:01 +0530

    THe logic is working little fine now
    Signed-off-by: anmoljain16 <ajanmoldosi@gmail.com>

--- src/app/ipctobns/page.jsx
+++ src/app/ipctobns/page.jsx
@@ -4,7 +4,6 @@ import Ipctobns from "@/components/ipctobns/form";
 export default async function ipctobns(){
     return (
         <>
-            <h1>Convert ipc to bns</h1>
             <Ipctobns/>
         </>
     );
