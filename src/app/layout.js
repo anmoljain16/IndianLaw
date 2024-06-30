513bee5f8f08077c781b21fe6f73f377ff15c118
anmoljain16 <ajanmoldosi@gmail.com>
2024-06-30 23:55:35 +0530

    Changing the UI for users to get section details easily
    Signed-off-by: anmoljain16 <ajanmoldosi@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import "./globals.css";
-import { Inter as FontSans } from "next/font/google"
+import {Inter as FontSans} from "next/font/google"
+import {GoogleAnalytics} from '@next/third-parties/google'
 
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,6 +17,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <GoogleAnalytics gaId="G-DDN7CMX1B3" />
+
     <body
         className={cn(
             "min-h-screen bg-background font-sans antialiased",
