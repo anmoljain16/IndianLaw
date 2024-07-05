83a1469a90cc0748fc64c0dbebde149462e35bd8
anmoljain16 <ajanmoldosi@gmail.com>
2024-07-06 01:28:30 +0530

    nvm Just improving little user experience by adding a arrow
    Signed-off-by: anmoljain16 <ajanmoldosi@gmail.com>

--- src/components/ipctobns/ComparisonTable.jsx
+++ src/components/ipctobns/ComparisonTable.jsx
@@ -8,6 +8,7 @@ const ComparisonTable = ({ ipcData, bnsData, crpcData, bnssData, ieaData, bsaDat
                 <tr   className="bg-gray-100">
                     {/*<th className="py-2 px-4 border-b">Aspect</th>*/}
                     <th className="py-2 px-4 border-b">IPC</th>
+                    <th className="py-2 px-4 border-b"></th>
                     <th className="py-2 px-4 border-b">BNS</th>
 
                 </tr>
@@ -16,6 +17,7 @@ const ComparisonTable = ({ ipcData, bnsData, crpcData, bnssData, ieaData, bsaDat
                 <tr align={"center"} >
                     {/*<td className="py-2 px-4 border-b font-semibold">Section</td>*/}
                     <td className="py-2 px-4 border-b">{ipcData?.sectionNo || '-'}</td>
+                    <td className="py-2 px-4 border-b">{`-->`}</td>
                     <td className="py-2 px-4 border-b">{bnsData?.sectionNo || '-'}</td>
 
                 </tr>
@@ -34,12 +36,15 @@ const ComparisonTable = ({ ipcData, bnsData, crpcData, bnssData, ieaData, bsaDat
                     <thead>
                     <tr  className="bg-gray-100">
                         <th className="py-2 px-4 border-b">CRPC</th>
+                        <th className="py-2 px-4 border-b"></th>
                         <th className="py-2 px-4 border-b">BNSS</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr align={"center"}>
                         <td className="py-2 px-4 border-b">{crpcData?.section || '-'}</td>
+                        <td className="py-2 px-4 border-b">{`-->`}</td>
+
                         <td className="py-2 px-4 border-b">{bnssData?.section || '-'}</td>
                     </tr>
                     {/*<tr>*/}
@@ -58,13 +63,16 @@ const ComparisonTable = ({ ipcData, bnsData, crpcData, bnssData, ieaData, bsaDat
                     <tr   className="bg-gray-100 ">
                         {/*<th className="py-2 px-4 border-b">Aspect</th>*/}
                         <th className="py-2 px-4 border-b">IEA</th>
+                        <th className="py-2 px-4 border-b"></th>
                         <th className="py-2 px-4 border-b">BSA</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr align={"center"} >
+                    <tr align={"center"}>
                         {/*<td className="py-2 px-4 border-b font-semibold justify-self-center">Section</td>*/}
                         <td className="py-2 px-4 border-b">{ieaData?.section || '-'}</td>
+                        <td className="py-2 px-4 border-b">{`-->`}</td>
+
                         <td className="py-2 px-4 border-b">{bsaData?.section || '-'}</td>
                     </tr>
                     {/*<tr>*/}
